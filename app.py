7cf33f67c7b429bbe4b6b3a157afd4c4c467241b
Jake Bryant <mail.jbryant1@gmail.com>
2026-06-26 12:41:44 -0400

    Remade frontend in tailwind to make it looks better, mmodified database to have the actual company data for the coming career fair

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, render_template, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
-import ai_api  # CHANGE: import ai_api directly instead of api_response
+import ai_api 
 
 app = Flask(__name__)
 
+
+#Home page
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
+#Resume output page
 @app.route('/match', methods=['POST'])
 def match_resume():
     user_resume_data = request.form.get("resume")
     print("Resume received")
 
-    # CHANGE: call ai_api's function instead of api_response's
     booth_recommendations = ai_api.get_booth_recommendations(user_resume_data)
 
     return render_template('match.html', resume=user_resume_data, booth_recomendations=booth_recommendations)
